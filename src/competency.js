163df1f2b026c467169c01925eb0b25dd3d35808
madisonhoover <madisonhoover4@gmail.com>
2020-04-05 20:29:25 -0400

    added evaluation page for competency, still needs linking

--- src/competency.js
+++ src/competency.js
@@ -4,18 +4,81 @@ import { chunk } from "lodash";
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Sidebar from "./Sidebar";
-import HomeIcon from "@material-ui/icons/Home";
-import Dashboard from "@material-ui/icons/Dashboard";
-import PeopleIcon from "@material-ui/icons/People";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
-import SettingsIcon from "@material-ui/icons/Settings";
-import color from "@material-ui/core/colors/red";
 import {withStyles} from "@material-ui/core/styles";
+
+const evalU  = {
+  title: "Evaluation Scale",
+  keyColumn: 'id',
+  data: {
+      columns: [ 
+          {
+              id: "score",
+              label: "Score",
+              colSize: "10px"
+          },
+          {
+              id: "perfcompl",
+              label: "Performance/Completion",
+              colSize: "110px"
+          },
+          {
+              id: "frequency",
+              label: "Frequency",
+              colSize: "60px"
+          },
+          {
+            id: "supportprompt",
+            label: "Support or prompt needed",
+            colSize: "80px"
+        },
+      ],
+      rows: [
+          {
+            score: "4",
+            perfcompl: "Does Well / Has mastered",
+            frequency: "Almost all the time",
+            supportprompt: "with no support",
+          },
+          {
+            score: "3",
+            perfcompl: "Can do by self and completes fairly well",
+            frequency: "Some (3/4) of the time",
+            supportprompt: "needs minimal support or prompt",
+          },
+          {
+            score: "2",
+            perfcompl: "Can do, but not well",
+            frequency: "1/4 of the time",
+            supportprompt: "needs partial support to complete", 
+          },
+          {
+            score: "1",
+            perfcompl: "Unable or cannot do",
+            frequency: "Almost never",
+            supportprompt: "full support required", 
+          },
+          {
+            score: "0",
+            perfcompl: "Unwilling (Can do but chooses not to)",
+            supportprompt: "Student expresses no interest in performing even with supports (Comment required)",
+          },
+          {
+            score: "N",
+            perfcompl: "Not evaluated by observation or not applicable"
+          },
+      ],
+  }, 
+  features: {
+    userConfiguration: {
+      copyToClipboard: true,
+    },
+  }
+}
 
 
 
 const options = {
-  title: "Excel Competencies, evaluated by: ",
+  title: "Competencies Evaluation for jsmith43:",
   dimensions: {
     datatable: {
       width: "100%",
@@ -36,6 +99,12 @@ const options = {
         editable: false
       },
       {
+        id: "Domain",
+        label: "Domain",
+        colSize: "150px",
+        editable: false
+      },
+      {
         id: "ImportanceLevel",
         label: "Importance Level",
         colSize: "80px",
@@ -44,12 +113,20 @@ const options = {
         values: ["High", "Medium", "Low"]
       },
       {
-        id: "Level",
-        label: "Level",
-        colSize: "20px",
+        id: "Difficulty",
+        label: "Difficulty",
+        colSize: "50px",
         editable: true,
         inputType: "select",
         values: ["B", "I", "A", "E"]
+      },
+      {
+        id: "Evaluation",
+        label: "Evaluation",
+        colSize: "50px",
+        editable: true,
+        inputType: "select",
+        values: ["N", "0", "1", "2", "3", "4"]
       },
       {
         id: "TrackedBy",
@@ -59,15 +136,15 @@ const options = {
         inputType: "input",
       },
       {
-        id: "TrackText",
-        label: "Text Response in Tracking System",
-        colSize: "100px",
+        id: "Comments",
+        label: "Comments",
+        colSize: "200px",
         editable: true,
         inputType: "input",
       },
       {
         id: "FreqOfTrack" ,
-        label: "Frequency of Tracking",
+        label: "Tracking Frequency",
         colSize: "80px",
         editable: true,
         inputType: "select",
@@ -77,26 +154,32 @@ const options = {
     rows: [
       {
         Competency: "12. Understands and demonstrates safe street crossing and other pedestrian laws",
-        Level: "B",
+        Difficulty: "B",
+        Domain: "Technology",
         ImportanceLevel : "High",
+        Evaluation: "N",
         TrackedBy: "Social Team",
-        TrackText: "",
+        Comments: "",
         FreqOfTrack: "Semester"
       },
       {
         Competency: "13. Understands and demonstrates when and where it is safe or unsafe to travel at night",
-        Level: "B",
-        ImportanceLevel : "Medium",
+        Difficulty: "B",
+        Domain: "Housing",
+        ImportanceLevel: "Medium",
+        Evaluation: "N",
         TrackedBy: "Social Team",
-        TrackText: "",
+        Comments: "",
         FreqOfTrack: "Semester"
       },
       {
         Competency: "14. Able to get to class and other familiar locations",
-        Level: "B",
+        Difficulty: "B",
+        Domain: "Health and Wellness",
         ImportanceLevel : "High",
+        Evaluation: "N",
         TrackedBy: "Social Team",
-        TrackText: "",
+        Comments: "",
         FreqOfTrack: "Semester"
       },
     ]
@@ -109,10 +192,9 @@ const options = {
     canSearch: true,
     canRefreshRows: true,
     canOrderColumns: true,
-    canSelectRow: true,
     canSaveUserConfiguration: true,
     userConfiguration: {
-      columnsOrder: ["Competency", "ImportanceLevel", "Level", "TrackedBy", "FreqOfTrack", "TrackText"],
+      columnsOrder: ["Competency", "Domain", "ImportanceLevel", "Difficulty", "Evaluation", "TrackedBy", "FreqOfTrack", "Comments"],
       copyToClipboard: true
     },
     rowsPerPage: {
@@ -126,24 +208,17 @@ const styles = theme => ({
   side: {
     margin: 0,
     padding: 0,
-    width: '200px',
+    width: '190px',
     backgroundColor : '#f1f1f1',
     position: 'fixed',
     height: '100%',
-    overflow: 'auto',
   },
   content: {
-  marginLeft: '200px',
+  marginLeft: '190px',
   padding: '1px 16px',
   height: '1000px',
 }
 });
-
-// const styles = theme => ({
-//   sideB: {
-//     float: left,
-//   },
-// });
 
 
 class App extends Component {
@@ -186,12 +261,9 @@ class App extends Component {
                      refreshRows={this.refreshRows}
                      actions={this.actionsRow}
           />
+          <Datatable options={evalU}/>
         </div>
-
-
-
-
-
+        
 
 
       </Container>
