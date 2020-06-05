3e5c05df46eb15a8e98c1ce6bd5614dcaf108f8e
jcorona8 <47695741+jcorona8@users.noreply.github.com>
2020-06-05 13:25:33 -0400

    Making domain match

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ Amplify.configure({
         userPoolWebClientId: '1lr290tbgl9c533rklc7ncgvhg',
 
         cookieStorage: {
-            domain: 'julia.d19x1ye7qes4du.amplifyapp.com',
+            domain: 'master.d36pda3rte31fa.amplifyapp.com',
             path: '/',
             expires: 365,
             secure: false
